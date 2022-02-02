a8310d6e2a8c8f79705376d4a2bc69dfb57f48ff
antoniom28 <antomarc@gmail.com>
2022-02-02 19:39:16 +0100

    tutte le milestone, no bonus, template da sistemar

--- src/components/section/FilmCards.vue
+++ src/components/section/FilmCards.vue
@@ -16,37 +16,37 @@
 
     <div class="back-face">
       <div v-if="films.original_title">
-        <h5>Titolo Originale: </h5>
+        <h5>Titolo Originale : &nbsp; </h5>
         <span>{{ films.original_title }}</span>
       </div>
 
       <div v-else>
-        <h5>Titolo Originale: </h5>
+        <h5>Titolo Originale : &nbsp; </h5>
         <span>{{ films.original_name }}</span>
       </div>
 
-      <div v-if="films.title != ''">
-        <h5>Titolo : </h5>
-        {{ films.title }}
+      <div v-if="films.title">
+        <h5>Titolo :&nbsp; </h5>
+        <span>{{ films.title }}</span>
       </div>
 
       <div v-else>
-          <h5>Titolo : </h5>
-          {{ films.name }}
+          <h5>Titolo :&nbsp; </h5>
+          <span>{{ films.name }}</span>
         </div>
 
 
       <img class="lang" :src="putFlag(films.original_language)" alt="" />
 
       <div class="vote">
-        <h5>Voto :</h5>
+        <h5>Voto : &nbsp;</h5>
         <span v-for="star in voteAverage(films.vote_average)" :key="star">
           <i class="fas fa-star"></i>
         </span>
       </div>
 
       <div class="overview">
-          <h5>overview :</h5>
+          <h5>overview : &nbsp;</h5>
           <span>{{ films.overview }}</span>
       </div>
     </div>
@@ -61,14 +61,11 @@ export default {
   },
   methods: {
     voteAverage(vote) {
-      // console.log(vote,'prima');
       vote /= 2;
       vote = Math.ceil(vote);
-      // console.log(vote);
       return vote;
     },
     putFlag(lang) {
-      //console.log(lang);
       let langArr = ["it", "en", "es"];
       let error = false;
       for (let i = 0; i < langArr.length; i++) {
@@ -89,6 +86,9 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../assets/style/partials/variables.scss';
+h5{
+    display: inline-block;
+}
 
 li {
   background-color: $bg_main_color;
@@ -100,17 +100,21 @@ li {
   width: 230px;
   height: 342px;
   position: relative;
-}
 
-.front-face{
+  [class*="-face"] {
     width: 94%;
     height: 94%;
     position: absolute;
     top: 3%;
     left: 3%;
+  }
+}
+
+.front-face{
     z-index: 999;
     background-color: $bg_main_color;
     transition: opacity 0.3s linear;
+
     img{
         width: 100%;
         box-shadow: 0 0 5px 1px whitesmoke;
@@ -122,21 +126,17 @@ li {
 }
 
 .back-face{
-    width: 94%;
-    height: 94%;
-    position: absolute;
-    top: 3%;
-    left: 3%;
-   /* background-color: red;*/
     padding: 10px;
+    height: 200px;
     overflow: hidden;
-}
 
-/*
-.overview{
-    background-color: skyblue;
-    overflow: hidden;
-}*/
+    .overview{
+         display: -webkit-box;
+        -webkit-line-clamp: 10;
+        -webkit-box-orient: vertical;  
+        overflow: hidden;
+    }
+}
 
 img.lang {
   width: 40px;
@@ -146,5 +146,6 @@ img.lang {
   color: rgb(202, 172, 0);
   margin: 0 2px;
   border-radius: 20px;
+  font-size: 1.5em;
 }
 </style>
